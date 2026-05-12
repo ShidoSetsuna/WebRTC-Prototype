9707aae31c281db83e021c9cc3b3130706073f87
Valdemar Andreas <vald3060@gmail.com>
2026-05-12 10:22:23 +0200

    Setip socket.io and basic room creation and joining

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 import 'dotenv/config';
 
 const app = express();
@@ -7,6 +8,32 @@ const server = createServer(app);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+    
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('a peer connected with ID: ' + socket.id);
+
+    socket.on('create-room', () => {
+        const roomId = Math.random().toString(36).substring(4, 8); // Generate a short random room ID with 4 characters
+        socket.join(roomId);
+        socket.emit('room-created', roomId);
+    });
+
+    socket.on('join-room', (roomId) => {
+        const room = io.sockets.adapter.rooms.get(roomId);
+        if (!room) {
+            socket.emit('room-not-found');
+            return;
+        }
+        socket.join(roomId);
+        socket.to(roomId).emit('peer-joined', socket.id);
+    })
 });
 
 server.listen(process.env.PORT, () => {
